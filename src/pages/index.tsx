42e3d71f89b1791d68d605df14052e4198a5e733
ariefardi <arief.kurnia@flockasia.com>
2024-10-24 06:42:49 +0700

    navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,33 +7,37 @@ import HomeContact from "@/components/home-contact";
 import HomeInfo from "@/components/home-info";
 import HomePrevwork from "@/components/home-prevwork";
 import HomeProcess from "@/components/home-process";
+import Navbar from "@/components/navbar/navbar";
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <div className="relative mobile:hidden">
-        <div className="w-[125px] h-[125px] absolute right-[20px] z-[50] top-[30vh]">
-          <Image src={ImageWidget} fill alt="widget" />
+    <div>
+      <Navbar />
+      <div className="flex flex-col relative">
+        <div className="relative mobile:hidden">
+          <div className="w-[125px] h-[125px] absolute right-[20px] z-[50] top-[30vh]">
+            <Image src={ImageWidget} fill alt="widget" />
+          </div>
         </div>
-      </div>
 
-      <div className="w-full max-w-screen mx-auto aspect-[375/361] block desktop:hidden relative">
-        <Image
-          src={ImageMobile} // Replace with your image path
-          alt="Responsive Image"
-          layout="responsive" // Ensure the image maintains the aspect ratio
-          width={375} // Width for the image
-          height={361} // Height for the image
-          objectFit="cover" // Ensure the image covers the entire container without distortion
-        />
-      </div>
-      <div className="text-xl desktop:block mobile:hidden h-screen w-full">
+        <div className="w-full max-w-screen mx-auto aspect-[375/361] block desktop:hidden relative">
+          <Image
+            src={ImageMobile} // Replace with your image path
+            alt="Responsive Image"
+            layout="responsive" // Ensure the image maintains the aspect ratio
+            width={375} // Width for the image
+            height={361} // Height for the image
+            objectFit="cover" // Ensure the image covers the entire container without distortion
+          />
+        </div>
+        {/* <div className="text-xl desktop:block mobile:hidden h-screen w-full">
         <Image alt="desktop-image" objectFit="cover" src={ImageDesktop} fill />{" "}
-      </div>
+      </div> */}
 
-      <HomeInfo />
-      <HomePrevwork />
-      <HomeProcess />
-      <HomeContact />
+        <HomeInfo />
+        <HomePrevwork />
+        <HomeProcess />
+        <HomeContact />
+      </div>
     </div>
   );
 }
